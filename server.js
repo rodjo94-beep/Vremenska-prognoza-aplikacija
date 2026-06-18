5ea1e428d646e9066b97be1e71307c780fb9f77a
rodjo94-beep <rodjo.94@gmail.com>
2026-06-18 17:18:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
+const app = express(); 
 const PORT = process.env.PORT || 3000; 
 
 let cachedWeather = null;
